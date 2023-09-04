8000735cec97de1c77eaf816aa40206349dbe8c7
Garimella Pavan Teja <115444753+GarPav123@users.noreply.github.com>
2023-09-04 15:03:03 +0530

    Update DB.ts

--- .github/managers/DB.ts
+++ .github/managers/DB.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
-import { rescheduleChallenges } from '../utils/rescheduleChallenges.js'
-import envHandler from './envHandler.js'
+//import { rescheduleChallenges } from '../utils/rescheduleChallenges.js'
+//import envHandler from './envHandler.js'
 
 const URL = envHandler("DATABASE_URL").replace('<password>', envHandler("DATABASE_PASSWORD"))
 
